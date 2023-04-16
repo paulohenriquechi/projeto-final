530ba4f713ea2b77ed1258cddadf01652adf80a0
Paulo Henrique Chi <paulo.henrique.chi@outlook.com>
2023-04-16 01:27:07 -0300

    layout pagina de musicas

--- frontend/src/views/SongsView.vue
+++ frontend/src/views/SongsView.vue
@@ -5,6 +5,26 @@
         text="Songs"
       />
       <div id="main-container">
+        <div id="song-container">
+          <!-- <div id="song-list-header"></div> -->
+            <ul class="song-list">
+              <li class="song">
+                <span class="song-header-image">#</span>
+                <span class="song-header-name">Song</span>
+                <span class="song-header-album">Album</span>
+                <span class="song-header-length">Length</span>
+              </li>
+              <li class="song" v-for="song in songs" :key="song.song_url">
+                <img class="album-image" :src="song.albumInfo[0].album_cover" :alt="song.album">
+                <router-link class="song-name" :to="'songs/'+song.song_url">{{ song.song_name }}</router-link>
+                <router-link class="album-name" :to="'albums/'+song.albumInfo[0].album_url">{{ song.album }}</router-link>
+                <span class="song-length">{{ song.length }}</span>
+              </li>
+            </ul>
+        </div>
+      </div>
+
+      <!-- <div id="main-container">
         <div class="song-container">
           <ul id="song-list">
           <li class="song header">
@@ -23,7 +43,10 @@
           </li>
         </ul>
         </div>
-      </div>
+      </div> -->
+
+
+
       <!-- <div v-for="song in songs" :key="song.song_url">
         <p> - 
            <router-link :to="'albums/'+song.album">{{ song.album }}</router-link>
@@ -63,8 +86,54 @@
     }
 </script>
 <style scoped>
-  
   #main-container{
+    border-top: 1px solid #999;
+  }
+  #song-container{
+    width: 50%;
+    margin: 0 auto;
+  }
+  .album-image{
+    width: 50px;
+    border: 1px solid white;
+  }
+  .song{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #999;
+    margin: 10px 0;
+    padding: 10px 0;
+  }
+  .song-length, .song-name, .album-name{
+    color: #999;
+  }
+  .album-image, .song-length{
+    width: 10%;
+  }
+  .song-name, .album-name{
+    width: 35%;
+    transition: all .5s ease-in-out;
+    
+  }
+  .song-name:hover, .album-name:hover{
+    text-decoration: underline;
+    color: #fff;
+  }
+  .song-header-image, .song-header-length{
+    width: 10%;
+    /* text-align: center; */
+  }
+  .song-header-image{
+    text-align: center;
+  }
+  .song-header-name, .song-header-album{
+    width: 35%;
+  }
+
+
+  
+  /* #main-container{
     border-top: 1px solid #999999;
   }
   .song-container{
@@ -96,6 +165,6 @@
 }
 .header{
   border: 1px solid white;
-}
+} */
 </style>
   
